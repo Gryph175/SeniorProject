6f044809468552683b886ae0193fa3fed89f5ad0
Gryph175 <griffin.sanford@gmail.com>
2020-02-24 23:03:43 -0500

    output title, price and rating

--- Python/scraper.py
+++ Python/scraper.py
@@ -7,6 +7,8 @@ NewEgg = "https://www.newegg.com/p/pl?d="
 Walmart = "https://www.walmart.com/search/?query=" 
 Target = "https://www.target.com/s?searchTerm="
 
+
+
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 
 file = open("WebURLs", "w")
@@ -49,10 +51,31 @@ for line in lines:
     if line.find( "href=")!= -1:
         if line.find("/gp/slredirect/picassoRedirect.html") == -1:
             if line.find("http") == -1:
-                line = line.replace('</a>, <a class="a-link-normal a-text-normal" href="', '')
+                line = line.replace('</a>, <a class="a-link-normal a-text-normal" href="', 'https://www.amazon.com')
                 line = line.replace('">', '')
                 file.write(line + "\n")
 file.close()
+
+file = open("WebURLs", "r")
+lines = file.readlines()
+
+
+
+for line in lines: 
+    line = line.strip()
+    productPage = pageURL(line)
+    productSoup = URLinput(productPage)
+
+    title = productSoup.find(id ="productTitle").get_text()
+    price = productSoup.find(id="priceblock_ourprice").get_text()
+    rating = productSoup.find(id="acrPopover").get_text()
+
+    print(title.strip())
+    print(price)
+    print(rating.strip())
+
+    
+
 
 
 
